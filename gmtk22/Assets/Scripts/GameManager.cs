e50bdea0fe21eef502aa85de94e10cc779087d8c
keyoni <56132773+keyoni@users.noreply.github.com>
2022-07-15 12:48:51 -0700

    grid puzzle game

---
+++ gmtk22/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField] private GridManager _gridManager;
+
+}
+
+// Start is called before the first frame update
+   
+   
+
+   
