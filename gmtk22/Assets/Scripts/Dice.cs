b8a0f63cb08482974cded038f926b596e3590ad3
keyoni <56132773+keyoni@users.noreply.github.com>
2022-07-15 17:39:10 -0700

    Dice Rollin

--- gmtk22/Assets/Scripts/Dice.cs
+++ gmtk22/Assets/Scripts/Dice.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Dice : MonoBehaviour {
 
     // Array of dice sides sprites to load from Resources folder
-    [SerializeField] Sprite[] diceSides;
+    [SerializeField] DiceFaceScriptiableObject[] diceSides;
+    
 
     // Reference to sprite renderer to change sprites
     private SpriteRenderer rend;
@@ -14,6 +17,7 @@ public class Dice : MonoBehaviour {
 
         // Assign Renderer component
         rend = GetComponent<SpriteRenderer>();
+        print($" {diceSides[0].value} ");
     }
 	
     // If you left click over the dice then RollTheDice coroutine is started
@@ -30,17 +34,17 @@ public class Dice : MonoBehaviour {
         int randomDiceSide = 0;
 
         // Final side or value that dice reads in the end of coroutine
-        int finalSide = 0;
+        String finalSide = "";
 
         // Loop to switch dice sides ramdomly
         // before final side appears. 20 itterations here.
         for (int i = 0; i <= 20; i++)
         {
             // Pick up random value from 0 to 5 (All inclusive)
-            randomDiceSide = Random.Range(0, 5);
+            randomDiceSide = Random.Range(0, 6);
 
             // Set sprite to upper face of dice from array according to random value
-            rend.sprite = diceSides[randomDiceSide];
+            rend.sprite = diceSides[randomDiceSide].faceSprite;
 
             // Pause before next itteration
             yield return new WaitForSeconds(0.05f);
@@ -48,7 +52,7 @@ public class Dice : MonoBehaviour {
 
         // Assigning final side so you can use this value later in your game
         // for player movement for example
-        finalSide = randomDiceSide + 1;
+        finalSide = diceSides[randomDiceSide].value;
 
         // Show final dice value in Console
         Debug.Log(finalSide);
---
+++ gmtk22/Assets/Scripts/DiceFaceScriptiableObject.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "New DiceFace", menuName= "ScriptableObject/DiceFace")]
+public class DiceFaceScriptiableObject : ScriptableObject
+{
+  public Sprite faceSprite;
+  public String value;
+
+}
